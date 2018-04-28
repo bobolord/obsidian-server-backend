5c048f68e9a7267642daaeb6d0d34d5e99815877
BoboLord_ <saisreedeep@outlook.com>
2018-04-28 12:43:14 +0530

    Jwt changes

--- middlewares/jwtMiddleware.go
+++ middlewares/jwtMiddleware.go
@@ -37,15 +37,32 @@ func JwtMiddleware() gin.HandlerFunc {
 	}
 }
 
-func CreateJwtToken(c *gin.Context) {
+func CreateJwtToken(c *gin.Context) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"user":           "sai@gmail.com",
 		"timeOfCreation": time.Now().Unix(),
 	})
 	fmt.Println("new token ", time.Now().Unix())
 	tokenString, err := token.SignedString(hmacSecret)
-	if err == nil {
+	if err != nil {
+		return "", fmt.Errorf("Error creating token")
+	} else {
 		setJwtCookie(c, tokenString)
+		refreshTokenCookie, err := c.Request.Cookie("REFRESH-TOKEN")
+		if err != nil {
+			setRefreshCookie(c, tokenString)
+		}
+		refreshTokenString := refreshTokenCookie.Value
+		refreshToken := JwtToken{}
+		token, err := jwt.ParseWithClaims(refreshTokenString, &refreshToken, func(token *jwt.Token) (interface{}, error) {
+			return hmacSecret, nil
+		})
+		if token.Valid == false {
+			utilities.Logout(c)
+			return "", fmt.Errorf("Authentication error")
+		} else {
+		}
+		return tokenString, nil
 	}
 }
 
@@ -80,6 +97,17 @@ func setJwtCookie(c *gin.Context, tokenString string) {
 		Name:     "JWT-TOKEN",
 		Value:    tokenString,
 		MaxAge:   utilities.Config.AppConfig.CsrfTokenExpiry,
+		Path:     "/",
+		Domain:   utilities.Config.AppConfig.Domain,
+		Secure:   false,
+		HttpOnly: false})
+}
+
+func setRefreshCookie(c *gin.Context, tokenString string) {
+	http.SetCookie(c.Writer, &http.Cookie{
+		Name:     "REFRESH-TOKEN",
+		Value:    tokenString,
+		MaxAge:   101474836, // 3 years
 		Path:     "/",
 		Domain:   utilities.Config.AppConfig.Domain,
 		Secure:   false,
